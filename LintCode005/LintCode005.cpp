7101952ece5537b34dda5b6e4276786374509525
reding <709307374@qq.com>
2015-08-22 12:43:50 +0800

    TODO 008

--- LintCode005/LintCode005.cpp
+++ LintCode005/LintCode005.cpp
@@ -1,9 +1,23 @@
 // LintCode005.cpp : 定义控制台应用程序的入口点。
 //
+/**
+在数组中找到第k大的元素
+样例
+给出数组[9,3,2,4,8]，第三大的元素是4
 
+给出数组 [1,2,3,4,5]，第一大的元素是5，第二大的元素是4，第三大的元素是3，以此类推
+
+注意
+你可以交换数组中的元素的位置
+
+挑战
+要求时间复杂度为O(n），空间复杂度为O(1）
+**/
 #include <iostream>
 #include <vector>
 #include <limits>
+#include <algorithm>
+#include <functional>
 using namespace std;
 
 class Solution {
@@ -15,8 +29,9 @@ public:
 	*/
 	int kthLargestElement(int k, vector<int> nums) {
 		// write your code here
-		
-		return 0;
+		sort(nums.begin(), nums.end(), greater<int>());
+		cout << nums[k - 1] << endl;
+		return nums[k - 1];
 	}
 };
 
@@ -28,7 +43,7 @@ void test1(Solution s)
 	{
 		nums.push_back(i);
 	}
-	if (s.kthLargestElement(3, nums) == 9998)
+	if (s.kthLargestElement(3, nums) == 9997)
 		cout << "test 1 passed!" << endl;
 	else
 		cout << "test 1 failed!" << endl;
@@ -41,7 +56,7 @@ void test2(Solution s)
 	{
 		nums.push_back(i);
 	}
-	if (s.kthLargestElement(2, nums) == 9999)
+	if (s.kthLargestElement(2, nums) == 999)
 		cout << "test 2 passed!" << endl;
 	else
 		cout << "test 2 failed!" << endl;
--- LintCode006/LintCode006.cpp
+++ LintCode006/LintCode006.cpp
@@ -1,11 +1,83 @@
 // LintCode006.cpp : 定义控制台应用程序的入口点。
 //
+/**
+合并两个排序的整数数组A和B变成一个新的数组。
 
-#include "stdafx.h"
+样例
+给出A=[1,2,3,4]，B=[2,4,5,6]，返回 [1,2,2,3,4,4,5,6]
 
+挑战
+你能否优化你的算法，如果其中一个数组很大而另一个数组很小？
+**/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	/**
+	* @param A and B: sorted integer array A and B.
+	* @return: A new sorted integer array
+	*/
+	vector<int> mergeSortedArray(vector<int> &A, vector<int> &B) {
+		// write your code here
+		vector<int> result;
+		int indexa = 0;
+		int indexb = 0;
+		while (indexa<A.size()&&indexb<B.size())
+		{
+			if (A[indexa] < B[indexb])
+			{
+				result.push_back(A[indexa]);
+				indexa++;
+			}
+			else
+			{
+				result.push_back(B[indexb]);
+				indexb++;
+			}
+		}
+		while (indexa < A.size())
+		{
+			result.push_back(A[indexa]);
+			indexa++;
+		}
+		while (indexb < B.size())
+		{
+			result.push_back(B[indexb]);
+			indexb++;
+		}
+		return result;
+	}
+};
+
+void test1()
+{
+	vector<int> a;
+	vector<int> b;
+	Solution s;
+	for (int i = 1;i <= 100;i++)
+	{
+		a.push_back(i);
+		b.push_back(i + 100);
+	}
+	vector<int> c = s.mergeSortedArray(a, b);
+	for (int j = 0;j < 200;j++)
+	{
+		if (c[j] != j+1)
+		{
+			cout << "test 1 failed!" << endl;
+			return;
+		}
+	}
+	cout << "test 1 passed!" << endl;
+}
 
 int main()
 {
+	test1();
+	system("pause");
     return 0;
 }
 
